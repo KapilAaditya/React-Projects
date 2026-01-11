42625b2c193ec725331078f2c3a4254e0e6f75b9
Aaditya kapil <aadityakapil2006@gmail.com>
2026-01-11 21:17:51 +0530

    Changes in Package.jason

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,12 +16,13 @@ export default function Navbar(props) {
     return (
 
         <>
-            <nav className={`navbar navbar-expand-lg border boder-2 border-white navbar-${props.mode} bg-${props.mode}`}>
+            <nav className={`navbar navbar-expand-lg border boder-2 border-white navbar-${props.mode} bg-${props.mode}`} >
+                <Link></Link>
 
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">
+                    {/* <Link className="navbar-brand" to="/TextForm">
                         {props.title}
-                    </Link>
+                    </Link> */}
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
